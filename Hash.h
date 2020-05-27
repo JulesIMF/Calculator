63cfcef14bdf773575c8890f30c0414332b4f0d6
JulesIMF <intermasterfly@yandex.ru>
2020-05-27 19:52:42 +0500

    Create Hash.h

---
+++ Hash.h
@@ -0,0 +1,9 @@
+//Hash.h is header file for application "Coder", which include heads of functions from "Hash.cpp".
+//Last edit was 01 December, 2014 year.
+//InterMasterFly`s app.
+
+long double code_1(long double Hash);
+long double code_2(long double Hash);
+long double code_3(long double Hash);
+long double code_4(long double Hash, int nI);
+long double code_end(long double Hash, int nI, int nQ);
